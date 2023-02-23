c8c07028ecd16249f9e41cdd439ff546a492eb98
jaemin <69967974+als982001@users.noreply.github.com>
2023-02-23 15:40:13 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 1.41 ms, Memory: 3.81 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/118668. 코딩 테스트 공부/코딩 테스트 공부.cpp
@@ -0,0 +1,179 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define MAX 151
+#define INF 987654321
+
+int reqAlp, reqCop;
+int record[MAX][MAX];
+vector<vector<int>> problemss;
+
+void Init()
+{
+    reqAlp = 0, reqCop = 0;
+    problemss.clear();
+
+    for (int a = 0; a < MAX; ++a)
+    {
+        for (int b = 0; b < MAX; ++b)
+            record[a][b] = INF;
+    }
+}
+
+int Smaller(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+int Check(int alp, int cop)
+{
+    if (record[alp][cop] < INF)
+        return record[alp][cop];
+
+    if (alp >= reqAlp && cop >= reqCop)
+        return 0;
+
+    int& curRecord = record[alp][cop];
+    
+    if (alp < 150)
+        curRecord = Smaller(curRecord, Check(alp + 1, cop) + 1);
+
+    if (cop < 150)
+        curRecord = Smaller(curRecord, Check(alp, cop + 1) + 1);
+
+    for (int p = 0; p < problemss.size(); ++p)
+    {
+        vector<int> problem = problemss[p];
+
+        int curReqAlp = problem[0];
+        int curReqCop = problem[1];
+        int rwdAlp = problem[2];
+        int rwdCop = problem[3];
+        int cost = problem[4];
+
+        if (alp >= curReqAlp && cop >= curReqCop)
+        {
+            int nxtAlp = alp + rwdAlp;
+            int nxtCop = cop + rwdCop;
+
+            if (nxtAlp >= 150)
+                nxtAlp = 150;
+            if (nxtCop >= 150)
+                nxtCop = 150;
+
+            curRecord = Smaller(curRecord, Check(nxtAlp, nxtCop) + cost);
+        }
+    }
+
+    return curRecord;
+}
+
+int solution(int alp, int cop, vector<vector<int>> problems)
+{
+    Init();
+    
+    problemss = problems;
+    
+    for (int p = 0; p < problems.size(); ++p)
+    {
+        vector<int> problem = problems[p];
+        int curReqAlp = problem[0];
+        int curReqCop = problem[1];
+
+        if (reqAlp < curReqAlp)
+            reqAlp = curReqAlp;
+        if (reqCop < curReqCop)
+            reqCop = curReqCop;
+    }
+
+    int answer = Check(alp, cop);
+
+    return answer;
+}
+
+/*
+#define MAX 151
+#define INF 987654321
+
+int targetAlp, targetCop;
+int dp[MAX][MAX];
+pair<pair<int, int>, int> record[MAX][MAX];
+vector<vector<int>> tempProblems;
+
+int Smaller(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+int Check(int alp, int cop)
+{
+    if (alp >= targetAlp && cop >= targetCop)
+        return 0;
+
+    if (dp[alp][cop] < INF)
+        return dp[alp][cop];
+
+    int& curDp = dp[alp][cop];
+
+    if (alp < targetAlp)
+        curDp = Smaller(curDp, Check(alp + 1, cop) + 1);
+
+    if (cop < targetCop)
+        curDp = Smaller(curDp, Check(alp, cop + 1) + 1);
+
+    for (int i = 0; i < tempProblems.size(); ++i)
+    {
+        vector<int> problem = tempProblems[i];
+
+        int reqAlp = problem[0];
+        int reqCop = problem[1];
+        int rwdAlp = problem[2];
+        int rwdCop = problem[3];
+        int cost = problem[4];
+
+        if (alp >= reqAlp && cop >= reqCop)
+        {
+            int nxtAlp = alp + rwdAlp >= MAX ? MAX - 1 : alp + rwdAlp;
+            int nxtCop = cop + rwdCop >= MAX ? MAX - 1 : cop + rwdCop;
+
+            curDp = Smaller(curDp, Check(nxtAlp, nxtCop) + cost);
+        }
+    }
+
+    return curDp;
+}
+
+int solution(int alp, int cop, vector<vector<int>> problems)
+{
+    tempProblems = problems;
+    targetAlp = 0;
+    targetCop = 0;
+
+    for (int i = 0; i < problems.size(); ++i)
+    {
+        vector<int> problem = problems[i];
+
+        int requiredAlp = problem[0];
+        int requiredCop = problem[1];
+
+        if (targetAlp < requiredAlp)
+            targetAlp = requiredAlp;
+
+        if (targetCop < requiredCop)
+            targetCop = requiredCop;
+    }
+
+    for (int a = 0; a < MAX; ++a)
+    {
+        for (int b = 0; b < MAX; ++b)
+            dp[a][b] = INF;
+    }
+
+    int answer = Check(alp, cop);
+
+
+    return answer;
+}
+*/
