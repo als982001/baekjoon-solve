1e0311e4c158e075a878365b6a9efd262b5c8bf4
jaemin <69967974+als982001@users.noreply.github.com>
2023-02-25 15:32:06 +0900

    [level 3] Title: 최고의 집합, Time: 0.01 ms, Memory: 3.61 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int s) {
+   vector<int> answer;
+
+	if (n > s)
+		return { -1 };
+
+	int num = s / n;
+	int r = s % n;
+
+	for (int i = 0; i < n; ++i)
+		answer.push_back(num);
+
+	for (int idx = answer.size() - 1, t = 0; t < r; ++t, --idx)
+		++answer[idx];
+	
+    return answer;
+}
