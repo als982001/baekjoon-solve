1d3591e5579fce43573ec5a213d5a14202641331
jaemin <69967974+als982001@users.noreply.github.com>
2023-03-04 16:01:23 +0900

    [level 3] Title: 등굣길, Time: 0.09 ms, Memory: 3.88 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-/*
 #define MAX 101
 #define MOD 1000000007
 
@@ -24,19 +23,19 @@ int Go(int r, int c)
 
 	if (visited[r][c] >= 0)
 		return visited[r][c];
+    
+    if (isPuddle[r][c])
+		return 0;
 		
 	if (r == 1 && c == 1)
 		return 1;
 
-	if (isPuddle[r][c])
-		return 0;
-
-	int& dp = visited[r][c];
-	dp = 0;
-
+    visited[r][c] = 0;
+    int& dp = visited[r][c];
+    
 	dp += ((Go(r - 1, c) + Go(r, c - 1)) % MOD);
-
-	return dp %= MOD;
+    
+    return dp;
 }
 
 int solution(int m, int n, vector<vector<int>> puddles)
@@ -47,14 +46,17 @@ int solution(int m, int n, vector<vector<int>> puddles)
 	for (int r = 0; r < MAX; ++r)
 	{
 		for (int c = 0; c < MAX; ++c)
-			visited[r][c] = -1;
+        {
+            visited[r][c] = -1;
+            isPuddle[r][c] = false;
+        }
 	}
 
 	for (int i = 0; i < puddles.size(); ++i)
 	{
 		vector<int> puddle = puddles[i];	
-		int puddleR = puddle[0];
-		int puddleC = puddle[1];
+		int puddleR = puddle[1];
+		int puddleC = puddle[0];
 
 		isPuddle[puddleR][puddleC] = true;
 	}
@@ -63,55 +65,4 @@ int solution(int m, int n, vector<vector<int>> puddles)
 
 	return answer;
 }
-*/
-
-// ================================
-#define MAX 111
-
-int R, C;
-int dp[MAX][MAX];
-bool puddle[MAX][MAX];
-
-int Is_In(int r, int c)
-{
-	return 1 <= r && r <= R && 1 <= c && c <= C;
-}
-
-int Solve(int r, int c)
-{
-	if (!Is_In(r, c))
-		return 0;
-
-	if (dp[r][c] >= 0)
-		return dp[r][c];
-
-	if (puddle[r][c])
-		return 0;
-
-	if (r == 1 && c == 1)
-		return 1;
-
-	dp[r][c] = 0;
-	int& curDp = dp[r][c];
-
-	return curDp += ((Solve(r - 1, c) + Solve(r, c - 1)) % 1000000007);
-}
-
-int solution(int m, int n, vector<vector<int>> puddles)
-{
-	R = n;
-	C = m;
-
-	for (int i = 0; i < puddles.size(); ++i)
-	{
-		vector<int> curPuddle = puddles[i];
-
-		puddle[curPuddle[1]][curPuddle[0]] = true;
-	}
-
-	memset(dp, -1, sizeof(dp));
-
-	int answer = Solve(R, C);
-
-	return answer;
-}
+ 
