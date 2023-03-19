87cd296f74d3ffe48dab010c19ab4b3add56bcfe
jaemin <69967974+als982001@users.noreply.github.com>
2023-03-19 22:21:42 +0900

    [level 2] Title: 단체사진 찍기, Time: 324.78 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv2/1835. 단체사진 찍기/단체사진 찍기.cpp
+++ 프로그래머스/lv2/1835. 단체사진 찍기/단체사진 찍기.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 #define MAX 8
 
+// 문제에서 주어지는 조건들의 타입형
 typedef struct
 {
 	int person1;
@@ -15,25 +16,28 @@ typedef struct
 	int distance;
 } Condition;
 
-int answer = 0;
-bool checked[MAX];
-int personPos[MAX];
-vector<int> line;
-vector<Condition> conditions;
-map<char, int> personIdx;
+int answer = 0;		// 답을 젖아할 변수
+bool checked[MAX];	// i번 사람이 줄을 섰는지를 저장할 변수
+int personPos[MAX];	// i번 사람의 현재 줄 위치
+vector<int> line;	// 사람들이 선 줄
+vector<Condition> conditions;	// 주어진 조건들을 저장할 배열
+map<char, int> personIdx;		// A 등의 사람 이름을 숫자로 바꾸기 위함
 
 void Check()
-{
-	if (line.size() >= MAX)
-	{
-		bool success = true;
+{	
+	// 2-2, 만약 모든 사람들이 줄을 섰다면, 이 줄으 기능한 줄인지 확인한다.
+	if (line.size() >= MAX)	
+	{	
+		bool success = true;	// 현재 줄이 가능한 줄이라면 true
 
+		// 2-2-1. 현재 각 사람들의 줄 위치를 저장한다.
 		for (int i = 0; i < line.size(); ++i)
 		{
 			int curPerson = line[i];
 			personPos[curPerson] = i;
 		}
 
+		// 2-2-2. 모든 조건들을 하나씩 검사하면서 가능한 줄인지 확인한다. 
 		for (int i = 0; i < conditions.size(); ++i)
 		{
 			Condition condition = conditions[i];
@@ -57,14 +61,17 @@ void Check()
 						break;
 			}
 
+			// 2-2-3. 만약 불가능한 줄이라면 return으로 빠져나간다.
 			if (success == false)
 				return;
 		}
 		
+		// 2-2-4. 만약 가능한 줄이라면 답을 +1 한다.
 		++answer;
 		return;
 	}
-
+	
+	// 2-1. 모든 사람들을 한 번씩 검사하면서, 줄을 서지 않았다면 줄을 세운다.
 	for (int i = 0; i < MAX; ++i)
 	{
 		if (checked[i] == false)
@@ -81,7 +88,8 @@ void Check()
 }
 
 int solution(int n, vector<string> data) 
-{
+{	
+	// 1. 전부 초기화한다.
 	answer = 0;
 
 	for (int i = 0; i < MAX; ++i)
@@ -102,7 +110,6 @@ int solution(int n, vector<string> data)
 	personIdx['R'] = 6;
 	personIdx['T'] = 7;
 
-
 	for (int i = 0; i < data.size(); ++i)
 	{
 		string curData = data[i];
@@ -116,112 +123,8 @@ int solution(int n, vector<string> data)
 		conditions.push_back(condition);
 	}
 
+	// 2. 순열 개념을 이용해 줄을 세운 후, 줄이 세워졌을 때 검사한다. 
 	Check();
 
 	return answer;
 }
-
-
-/*
-using namespace std;
-
-int lineNum;
-bool check[8];
-vector<int> line;
-vector<string> datas;
-map<char, int> characterNum;
-
-void Line_Up(int cnt)
-{
-    if (cnt == 8)
-    {
-        for (int dataIdx = 0; dataIdx < datas.size(); ++dataIdx)
-        {
-            string curData = datas[dataIdx];
-
-            int curChar1 = characterNum[curData[0]];
-            int curChar2 = characterNum[curData[2]];
-            int between = curData[4] - '0' + 1;
-            
-            int curChar1Pos = 0, curChar2Pos = 0;
-
-            for (int i = 0; i < line.size(); ++i)
-            {
-                if (line[i] == curChar1)
-                    curChar1Pos = i;
-                else if (line[i] == curChar2)
-                    curChar2Pos = i;
-            }
-
-            switch (curData[3])
-            {
-                case '=':
-                {
-                    if (abs(curChar1Pos - curChar2Pos) != between)
-                        return;
-                    break;
-                }
-                case '>':
-                {
-                    if (abs(curChar1Pos - curChar2Pos) <= between)
-                        return;
-                    break;
-                }
-                case '<':
-                {
-                    if (abs(curChar1Pos - curChar2Pos) >= between)
-                        return;
-                    break;
-                }
-            }
-        }
-
-        ++lineNum;
-
-        return;
-    }
-
-    for (int i = 0; i < 8; ++i)
-    {
-        if (!check[i])
-        {
-            check[i] = true;
-            line.push_back(i);
-
-            Line_Up(cnt + 1);
-            
-            line.pop_back();
-            check[i] = false;
-        }
-    }
-}
-
-int solution(int n, vector<string> data) {
-    int answer = 0;
-
-    lineNum = 0;
-    for (int i = 0; i < 8; ++i)
-        check[i] = false;
-    line.clear();
-    datas.clear();
-    characterNum.clear();
-
-
-    datas = data;
-
-    characterNum['A'] = 0;
-    characterNum['C'] = 1;
-    characterNum['F'] = 2;
-    characterNum['J'] = 3;
-    characterNum['M'] = 4;
-    characterNum['N'] = 5;
-    characterNum['R'] = 6;
-    characterNum['T'] = 7;
-
-    Line_Up(0);
-
-    answer = lineNum;
-
-    return answer;
-}
-*/
