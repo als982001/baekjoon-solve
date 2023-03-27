2f85c7dab1f6bd6259e3ed9d960fcb1f1c18be1e
jaemin <69967974+als982001@users.noreply.github.com>
2023-03-27 17:41:19 +0900

    [level 3] Title: N으로 표현, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
@@ -5,14 +5,16 @@ using namespace std;
 #define MAX 8
 
 int targetNum;
-int minCount = MAX + 1;
+int minCount = MAX + 1; // 문제의 조건인 '최솟값이 8보다 크면 -1을 return 합니다.'를 위해 설정
 int ns[MAX + 1];
 
 void Check(int curCount, int curNum)
-{
+{   
+    // 2-1. 만약, 현재 N을 사용한 횟수가 저장되어 있는 최소 사용 횟수보다 많거나 같으면 이 경우의 검사를 그만둔다.
     if (curCount >= minCount)
         return;
     
+    // 2-2. 만약, 현재 숫자가 문제의 목표 숫자와 같다면, 답을 갱신할 수 있는 경우는 갱신하고 검사를 그만둔다.
     if (curNum == targetNum)
     {
         if (minCount > curCount)
@@ -21,6 +23,7 @@ void Check(int curCount, int curNum)
         return;
     }
 
+    // 2-3. 1번에서 미리 준비했던 그냥 붙이기로 만들어진 숫자들을 1개부터 8개까지의 경우를 사칙연산으로 계산하여 검사를 진행한다.
     for (int numberOfUses = 1; numberOfUses <= MAX; ++numberOfUses)
     {
         int nxtCount = curCount + numberOfUses;
@@ -35,6 +38,7 @@ void Check(int curCount, int curNum)
 
 int solution(int N, int number) 
 {
+    // 1. 우선 사칙연산 외에 숫자를 이용가능한 방법인 그냥 붙이기(만약 N이 5라면 5, 55, 555 같은 경우)로 만들 수 있는 경우를 미리 준비한다.
     targetNum = number;
 
     ns[1] = N;
@@ -42,6 +46,7 @@ int solution(int N, int number)
     for (int i = 2; i <= MAX; ++i)
         ns[i] = (ns[i - 1] * 10) + N;
 
+    // 2. N을 하나 사용하는 경우부터 차근차근 살펴본다.
     Check(0, 0);
 
     if (minCount > MAX)
@@ -49,54 +54,3 @@ int solution(int N, int number)
     else
         return minCount;
 }
-
-/*
-int n, num;
-int maxCnt = 9;
-int arr[9];
-
-void Make_Array() {
-
-    int input = n;
-
-    for (int i = 1; i <= 8; ++i) {
-        arr[i] = input;
-        input = (input * 10) + n;
-    }
-}
-
-void Find_Answer(int curCnt, int curNum) {
-
-    if (curCnt >= maxCnt)
-        return;
-
-    if (curNum == num) {
-        maxCnt = curCnt;
-        return;
-    }
-
-    for (int numOfUse = 1; numOfUse <= 8; ++numOfUse) {
-
-        int nxtCnt = curCnt + numOfUse;
-
-        Find_Answer(nxtCnt, curNum + arr[numOfUse]);
-        Find_Answer(nxtCnt, curNum - arr[numOfUse]);
-        Find_Answer(nxtCnt, curNum * arr[numOfUse]);
-        Find_Answer(nxtCnt, curNum / arr[numOfUse]);
-    }
-}
-
-int solution(int N, int number) {
-    
-    n = N;
-    num = number;
-
-    Make_Array();
-    Find_Answer(0, 0);
-
-    if (maxCnt > 8)
-        maxCnt = -1;
-
-    return maxCnt;
-}
-*/
