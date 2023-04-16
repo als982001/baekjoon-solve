17ab7fe546b7381f99102f75786ec4b34dc4e65d
jaemin <69967974+als982001@users.noreply.github.com>
2023-04-16 22:14:19 +0900

    [level 2] Title: 전화번호 목록, Time: 397.32 ms, Memory: 298 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -0,0 +1,79 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define MAX 11
+
+struct Trie 
+{
+    bool isFinish;
+    Trie* node[MAX];
+    
+    Trie()
+    {
+        this->isFinish = false;
+        
+        for (int i = 0; i < MAX; ++i)
+                this->node[i] = NULL;
+    }
+    
+    void Insert(string str, int idx);
+    bool IsPrefix(string str, int idx);
+};
+
+void Trie::Insert(string str, int idx)
+{
+    if (idx >= str.size())
+    {
+        this->isFinish = true;
+        return;
+    }
+    
+    int num = str[idx] - '0';
+    
+    if (this->node[num] == NULL)
+        this->node[num] = new Trie();
+    this->node[num]->Insert(str, idx + 1);
+}
+
+bool Trie::IsPrefix(string str, int idx)
+{
+    if (idx >= str.size())
+    {
+        for (int i = 0; i < MAX; ++i)
+        {
+            if (this->node[i] != NULL)
+                return false;
+        }
+        
+        return true;
+    }
+    
+    int num = str[idx] - '0';
+    
+    return this->node[num]->IsPrefix(str, idx + 1);
+}
+
+bool solution(vector<string> phone_book) {
+    Trie* trie = new Trie();
+    
+    for (int i = 0; i < phone_book.size(); ++i)
+    {
+        string phoneNum = phone_book[i];
+        
+        trie->Insert(phoneNum, 0);
+    }
+    
+    for (int i = 0; i < phone_book.size(); ++i)
+    {
+        string phoneNum = phone_book[i];
+        
+        bool result = trie->IsPrefix(phoneNum, 0);
+        
+        if (result == false)
+            return false;
+    }
+    
+    return true;
+}
