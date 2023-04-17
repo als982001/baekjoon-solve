d9121e83ef1246ce7e55f9de5a760c777f4f122f
jaemin <69967974+als982001@users.noreply.github.com>
2023-04-17 21:19:05 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.cpp
@@ -1,5 +1,77 @@
 #include <string>
 #include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+vector<string> genreList;
+priority_queue<pair<int, string>> sortedGenres;
+map<string, int> playCount;
+map<string, priority_queue<pair<int, int>>> songsByGenre;
+
+void Record(string genre, int play, int idx)
+{
+    songsByGenre[genre].push({ -play, idx });
+    
+    if (songsByGenre[genre].size() > 2)
+        songsByGenre[genre].pop();
+}
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+    
+    for (int idx = 0; idx < genres.size(); ++idx)
+    {
+        string genre = genres[idx];
+        int play = plays[idx];
+        
+        if (playCount[genre] == 0)
+        {
+            playCount[genre] = play;
+            genreList.push_back(genre);
+        }
+        else
+            playCount[genre] += play;
+        
+        Record(genre, play, idx);
+    }
+    
+    for (int i = 0; i < genreList.size(); ++i)
+    {
+        string genre = genreList[i];
+        int totalPlay = playCount[genre];
+        
+        sortedGenres.push({ totalPlay, genre });
+    }
+    
+    while(!sortedGenres.empty())
+    {
+        string genre = sortedGenres.top().second;
+        sortedGenres.pop();
+        
+        stack<int> songIndexes;
+        
+        while(!songsByGenre[genre].empty())
+        {
+            songIndexes.push(songsByGenre[genre].top().second);
+            songsByGenre[genre].pop();
+        }
+        
+        while(!songIndexes.empty())
+        {
+            answer.push_back(songIndexes.top());
+            songIndexes.pop();
+        }
+    }
+    
+    return answer;
+}
+
+/*
+#include <string>
+#include <vector>
 #include <algorithm>
 #include <map>
 using namespace std;
@@ -70,3 +142,4 @@ vector<int> solution(vector<string> genres, vector<int> plays)
 
 	return answer;
 }
+*/
