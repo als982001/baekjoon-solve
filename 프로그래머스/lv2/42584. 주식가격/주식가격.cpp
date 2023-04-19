e2e260f8610691469c8d5d309fba7e2c50168c9d
jaemin <69967974+als982001@users.noreply.github.com>
2023-04-19 22:38:06 +0900

    [level 2] Title: 주식가격, Time: 15.77 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.cpp
@@ -26,32 +26,3 @@ vector<int> solution(vector<int> prices) {
     
     return answer;
 }
-
-/*
-#include <string>
-#include <vector>
-
-using namespace std;
-
-vector<int> solution(vector<int> prices) {
-    vector<int> answer;
-    
-    for (int current = 0; current < prices.size() - 1; ++current) {
-
-        int target = current + 1;
-        for (; target < prices.size(); ++target) {
-            
-            if (prices[current] > prices[target])
-                break;
-        }
-
-        if (target == prices.size())
-            answer.push_back(target - current - 1);
-        else
-            answer.push_back(target - current);
-    }
-    answer.push_back(0);
-    
-    return answer;
-}
-*/
