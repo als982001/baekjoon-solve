b1a094f3439f6adb1f113ee08153b6e8be5b9a40
jaemin <69967974+als982001@users.noreply.github.com>
2023-04-22 14:30:10 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.61 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,5 +1,83 @@
 #include <string>
 #include <vector>
+
+using namespace std;
+
+#define MAX 101
+
+int nodesNum;
+bool checked[MAX];
+vector<int> links[MAX];
+
+void Link(vector<vector<int>> wires, int cutWire)
+{
+    for (int i = 0; i < wires.size(); ++i)
+    {
+        if (i == cutWire)
+            continue;
+        
+        vector<int> wire = wires[i];
+        
+        int nodeA = wire[0];
+        int nodeB = wire[1];
+        
+        links[nodeA].push_back(nodeB);
+        links[nodeB].push_back(nodeA);
+    }
+}
+
+void Check(int node)
+{
+    if (checked[node] == true)
+        return;
+    checked[node] = true;
+    
+    ++nodesNum;
+    
+    for (int i = 0; i < links[node].size(); ++i)
+    {
+        int nxtNode = links[node][i];
+        Check(nxtNode);
+    }
+}
+
+int solution(int n, vector<vector<int>> wires) {
+    int answer = 500;
+    
+    for (int cutWire = 0; cutWire < wires.size(); ++cutWire)
+    {
+        for (int node = 0; node < MAX; ++node)
+        {
+            links[node].clear();
+            checked[node] = false;
+        }
+        
+        Link(wires, cutWire);
+        
+        vector<int> nodesNums;
+        
+        for (int node = 1; node <= n; ++node)
+        {
+            if (checked[node] == false)
+            {
+                nodesNum = 0;
+                Check(node);
+                nodesNums.push_back(nodesNum);
+            }
+        }
+        
+        int diff = abs(nodesNums[0] - nodesNums[1]);
+        
+        if (answer > diff)
+            answer = diff;
+    }
+    
+    return answer;
+}
+
+/*
+#include <string>
+#include <vector>
 #include <map>
 #include <queue>
 
@@ -90,3 +168,4 @@ int solution(int n, vector<vector<int>> wires)
 
     return answer;
 }
+*/
