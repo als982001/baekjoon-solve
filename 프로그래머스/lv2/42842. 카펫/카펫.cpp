7b0f5275b7cb64a266ac29b20de17945323ab777
jaemin <69967974+als982001@users.noreply.github.com>
2023-04-22 10:23:19 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.cpp
+++ 프로그래머스/lv2/42842. 카펫/카펫.cpp
@@ -17,39 +17,3 @@ vector<int> solution(int brown, int yellow) {
             return { width + 2, height + 2 };
     }
 }
-
-/*
-#include <string>
-#include <vector>
-
-using namespace std;
-
-void Swap(int& a, int& b) {
-
-    int temp = a;
-    a = b;
-    b = temp;
-}
-
-vector<int> solution(int brown, int yellow) {
-    vector<int> answer;
-    
-    int maxHorizontal = (brown - 6) / 2;
-
-    for (int horizontal = 1; horizontal <= maxHorizontal; ++horizontal) {
-
-        int vertical = (brown - 4 - (2 * horizontal)) / 2;
-        
-        if (horizontal * vertical == yellow) {
-
-            if (vertical > horizontal)
-                Swap(vertical, horizontal);
-
-            answer.push_back(horizontal + 2);
-            answer.push_back(vertical + 2);
-
-            return answer;
-        }
-    }
-}
-*/
