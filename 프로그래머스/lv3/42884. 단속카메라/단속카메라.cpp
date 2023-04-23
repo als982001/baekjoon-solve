1d641b2bafdd2cc6cb57fb762cc50a524af40e31
jaemin <69967974+als982001@users.noreply.github.com>
2023-04-23 10:38:51 +0900

    [level 3] Title: 단속카메라, Time: 0.63 ms, Memory: 4.05 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.cpp
@@ -26,33 +26,3 @@ int solution(vector<vector<int>> routes) {
     
     return cameraNum;
 }
-
-
-/*
-#include <string>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-int solution(vector<vector<int>> routes) {
-    int answer = 1;
-
-    sort(routes.begin(), routes.end());
-
-    int lastPos = routes[0][1];
-
-    for (int i = 1; i < routes.size(); ++i)
-    {
-        if (routes[i][1] < lastPos)
-            lastPos = routes[i][1];
-        else if (lastPos < routes[i][0])
-        {
-            ++answer;
-            lastPos = routes[i][1];
-        }
-    }
-
-    return answer;
-}
-*/
