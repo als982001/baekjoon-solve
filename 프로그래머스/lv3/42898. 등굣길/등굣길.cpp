0af08319cb2cf66b6a3b1b7471cb33fd2614eea6
jaemin <69967974+als982001@users.noreply.github.com>
2023-04-23 13:39:27 +0900

    [level 3] Title: 등굣길, Time: 0.11 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.cpp
@@ -1,5 +1,69 @@
 #include <string>
 #include <vector>
+
+using namespace std;
+
+#define MAX 101
+#define MOD 1000000007
+
+int R, C;
+bool isPuddle[MAX][MAX];
+int dp[MAX][MAX];
+
+bool IsIn(int r, int c)
+{
+    return 1 <= r && r <= R && 1 <= c && c <= C;
+}
+
+int Go(int r, int c)
+{
+    if (IsIn(r, c) == false)
+        return 0;
+    
+    if (isPuddle[r][c] == true)
+        return 0;
+    
+    if (dp[r][c] > -1)
+        return dp[r][c];
+    
+    if (r == 1 && c == 1)
+        return 1;
+    
+    int& curDp = dp[r][c];
+    curDp = 0;
+    
+    curDp += (Go(r - 1, c) + Go(r, c - 1)) % MOD;
+    
+    return curDp % MOD;
+    
+}
+
+int solution(int m, int n, vector<vector<int>> puddles) {
+    R = n;
+    C = m;
+    
+    for (int i = 0; i < puddles.size(); ++i)
+    {
+        int puddleR = puddles[i][1];
+        int puddleC = puddles[i][0];
+        
+        isPuddle[puddleR][puddleC] = true;
+    }
+    
+    for (int r = 0; r < MAX; ++r)
+    {
+        for (int c = 0; c < MAX; ++c)
+            dp[r][c] = -1;
+    }
+    
+    int answer = Go(R, C);
+    
+    return answer;
+}
+
+/*
+#include <string>
+#include <vector>
 #include <memory.h>
 
 using namespace std;
@@ -48,7 +112,6 @@ int solution(int m, int n, vector<vector<int>> puddles)
 		for (int c = 0; c < MAX; ++c)
         {
             visited[r][c] = -1;
-            isPuddle[r][c] = false;
         }
 	}
 
@@ -65,4 +128,4 @@ int solution(int m, int n, vector<vector<int>> puddles)
 
 	return answer;
 }
- 
+*/
