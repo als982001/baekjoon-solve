dfc18acb6e0fd1ee5a4bbce0297a7f9265c662fb
jaemin <69967974+als982001@users.noreply.github.com>
2023-04-23 11:17:33 +0900

    [level 3] Title: N으로 표현, Time: 28.03 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
@@ -1,5 +1,62 @@
 #include <string>
 #include <vector>
+
+using namespace std;
+
+#define MAX 8
+
+int targetNumber;
+int answer = 987654321;
+int dp[32111];
+int ns[MAX + 1];
+
+void Init(int N)
+{
+    ns[1] = N;
+    
+    for (int i = 2; i <= MAX; ++i)
+        ns[i] = ns[i - 1] * 10 + N;
+}
+
+void Check(int count, int number)
+{   
+    if (count > MAX)
+        return;
+    
+    if (number == targetNumber)
+    {
+        if (answer > count)
+            answer = count;
+        
+        return;
+    }
+    
+    for (int usageCount = 1; usageCount <= MAX; ++usageCount)
+    {
+        Check(count + usageCount, number + ns[usageCount]);
+        Check(count + usageCount, number - ns[usageCount]);
+        Check(count + usageCount, number * ns[usageCount]);
+        Check(count + usageCount, number / ns[usageCount]);
+    }
+}
+
+int solution(int N, int number) 
+{
+    targetNumber = number;
+    Init(N);
+    
+    Check(0, 0);
+    
+    if (answer == 987654321)
+        return -1;
+    return answer;
+}
+
+
+
+/*
+#include <string>
+#include <vector>
 using namespace std;
 
 #define MAX 8
@@ -54,3 +111,4 @@ int solution(int N, int number)
     else
         return minCount;
 }
+*/
