abffea9e735e3fabd25f4a975b58434b10f37f9b
jaemin <69967974+als982001@users.noreply.github.com>
2023-04-30 23:04:57 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.cpp
@@ -4,6 +4,65 @@
 #include <map>
 using namespace std;
 
+bool CanChange(string from, string to)
+{
+    bool changed = false;
+    
+    for (int i = 0; i < from.size(); ++i)
+    {
+        if (from[i] != to[i])
+        {
+            if (changed == true)
+                return false;
+            changed = true;
+        }
+    }
+    
+    return true;
+}
+
+int solution(string begin, string target, vector<string> words) {
+
+    map<string, bool> check;
+    queue<pair<string, int>> q;
+    
+    q.push({ begin, 0 });
+    check[begin] = true;
+    
+    while(!q.empty())
+    {
+        string curWord = q.front().first;
+        int curTime = q.front().second;
+        q.pop();
+        
+        if (curWord == target)
+            return curTime;
+        
+        for (int i = 0; i < words.size(); ++i)
+        {
+            string nxtWord = words[i];
+            
+            if (check[nxtWord] == true || curWord == nxtWord)
+                continue;
+            
+            if (CanChange(curWord, nxtWord))
+            {
+                check[nxtWord] = true;
+                q.push({ nxtWord, curTime + 1 });
+            }
+        }
+    }
+    
+    return 0;
+}
+
+/*
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+using namespace std;
+
 bool CanChange(string wordA, string wordB)
 {
 	int differentAlpNum = 0;
@@ -56,3 +115,4 @@ int solution(string begin, string target, vector<string> words)
 
 	return 0;
 }
+*/
