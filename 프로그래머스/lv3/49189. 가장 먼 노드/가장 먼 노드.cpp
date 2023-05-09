523f92e7f3e35feda4d3443b6d564118764e5c1b
jaemin <69967974+als982001@users.noreply.github.com>
2023-05-09 09:43:08 +0900

    [level 3] Title: 가장 먼 노드, Time: 8.63 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.cpp
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.cpp
@@ -1,153 +1,61 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <map>
-
+#define MAX 20001
 using namespace std;
 
-#define MAX 20011
-#define START 1
-
-int dist[MAX];
+int maxDist = 0;
+int answer = 0;
+bool visited[MAX];
 vector<int> links[MAX];
 
-int Check(int nodeNum)
+void Check(int startNode)
 {
-    int maxDist = -1;
-    int maxDistNum = 0;
-
-    priority_queue<pair<int, int>> pq;
-    pq.push({ START, 0 });
-
-    dist[START] = 0;
-
-    while(!pq.empty())
+    queue<pair<int, int>> q;
+    q.push({ startNode, 0 });
+    visited[startNode] = true;
+    
+    while(!q.empty())
     {
-        int curNode = pq.top().first;
-        int curDist = -pq.top().second;
-        pq.pop();
-
-        if (dist[curNode] < curDist)
-            continue;
-
+        int curNode = q.front().first;
+        int curDist = q.front().second;
+        q.pop();
+        
+        if (maxDist == curDist)
+            ++answer;
+        else if (maxDist < curDist)
+        {
+            maxDist = curDist;
+            answer = 1;
+        }
+        
         for (int i = 0; i < links[curNode].size(); ++i)
         {
             int nxtNode = links[curNode][i];
-            int nxtDist = curDist + 1;
-
-            if (dist[nxtNode] > nxtDist)
+            
+            if (visited[nxtNode] == false)
             {
-                dist[nxtNode] = nxtDist;
-                pq.push({ nxtNode, -nxtDist });
+                visited[nxtNode] = true;
+                q.push({ nxtNode, curDist + 1 });
             }
         }
     }
-
-    for (int node = 2; node <= nodeNum; ++node)
-    {
-        if (maxDist < dist[node])
-        {
-            maxDist = dist[node];
-            maxDistNum = 1;
-        }
-        else if (maxDist == dist[node])
-            ++maxDistNum;
-    }
-
-    return maxDistNum;
-}
-
-int solution(int n, vector<vector<int>> vertex) 
-{
-    for (int i = 0; i < MAX; ++i)
-        dist[i] = MAX + MAX + MAX;
-
-    for (int i = 0; i < vertex.size(); ++i)
-    {
-        int nodeA = vertex[i][0];
-        int nodeB = vertex[i][1];
-
-        links[nodeA].push_back(nodeB);
-        links[nodeB].push_back(nodeA);
-    }
-
-    int answer = Check(n);
-
-    return answer;
-}
-
-/*
-#define MAX 20011
-#define INF 987654321
-using namespace std;
-
-int nodeNum;
-int dist[MAX];
-vector<pair<int, int>> link[MAX];
-
-void Reset_Dist(int n) {
-    for (int i = 1; i <= n; ++i)
-        dist[i] = INF;
 }
 
 int solution(int n, vector<vector<int>> edge) {
-
-    int answer = 0;
-
-    int maxDist = 0;
-    vector<int> farNode;
-
-    for (int i = 0; i < edge.size(); ++i) {
-
-        int a = edge[i][0];
-        int b = edge[i][1];
-
-        link[a].push_back(make_pair(b, 1));
-        link[b].push_back(make_pair(a, 1));
-    }
-
-    int start = 1;
-    Reset_Dist(n);
-    dist[start] = 0;
-
-    priority_queue<pair<int, int>> pq;
-    pq.push(make_pair(start, 0));
-
-    while (!pq.empty()) {
-
-        int curNode = pq.top().first;
-        int curDist = -pq.top().second;
-        pq.pop();
-
-        if (curDist > dist[curNode])
-            continue;
-
-        for (int i = 0; i < link[curNode].size(); ++i) {
-
-            int nxtNode = link[curNode][i].first;
-            int nxtDist = curDist + link[curNode][i].second;
-
-            if (dist[nxtNode] > nxtDist) {
-
-                dist[nxtNode] = nxtDist;
-                pq.push(make_pair(nxtNode, -nxtDist));
-            }
-        }
+    for (int node = 0; node < MAX; ++node)
+        visited[node] = false;
+    
+    for (int i = 0; i < edge.size(); ++i)
+    {
+        int nodeA = edge[i][0];
+        int nodeB = edge[i][1];
+        
+        links[nodeA].push_back(nodeB);
+        links[nodeB].push_back(nodeA);
     }
     
-    for (int i = 2; i <= n; ++i) {
-
-        if (maxDist < dist[i]) {
-            maxDist = dist[i];
-            farNode.clear();
-            farNode.push_back(i);
-        }
-        else if (maxDist == dist[i])
-            farNode.push_back(i);
-    }
-
-    answer = farNode.size();
-
+    Check(1);
+    
     return answer;
 }
-*/
