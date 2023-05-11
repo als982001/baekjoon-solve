ad87cd3253d95d7287571ad8064983f528a674cf
jaemin <69967974+als982001@users.noreply.github.com>
2023-05-11 09:23:56 +0900

    [Bronze IV] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <memory.h>
+#include <deque>
+#include <cmath>
+#include <stack>
+#include <cstring>
+#include <typeinfo>
+#include <iomanip>
+#include <limits.h> 
+#include <map>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int main()
+{
+    long long a, b;
+    
+    cin >> a >> b;
+    
+    cout << (a == b ? 1 : 0) << endl;
+}
