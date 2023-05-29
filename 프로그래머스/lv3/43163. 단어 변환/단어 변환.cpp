45151073424466cb05a145a53846b5a2d3655001
jaemin <69967974+als982001@users.noreply.github.com>
2023-05-29 21:46:25 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.cpp
@@ -1,6 +1,64 @@
 #include <string>
 #include <vector>
 #include <queue>
+#define MAX 11
+using namespace std;
+
+bool checked[MAX];
+
+bool CanChange(string from, string to)
+{
+    int diffNum = 1;
+    
+    for (int i = 0; i < from.size(); ++i)
+    {
+        if (from[i] != to[i])
+        {
+            if (--diffNum < 0)
+                return false;
+        }
+    }
+    
+    return true;
+}
+
+int solution(string begin, string target, vector<string> words) {
+    int answer = 0;
+
+    queue<pair<string, int>> q;
+    q.push({ begin, 0 });
+    
+    while(!q.empty())
+    {
+        string curWord = q.front().first;
+        int changeTime = q.front().second;
+        q.pop();
+        
+        if (curWord == target)
+        {
+            answer = changeTime;
+            break;
+        }
+        
+        for (int i = 0; i < words.size(); ++i)
+        {
+            string nxtWord = words[i];
+            
+            if (checked[i] == false && CanChange(curWord, nxtWord) == true)
+            {
+                checked[i] = true;
+                q.push({ nxtWord, changeTime + 1 });
+            }
+        }
+    }
+    
+    return answer;
+}
+
+/*
+#include <string>
+#include <vector>
+#include <queue>
 #include <map>
 using namespace std;
 
@@ -55,3 +113,4 @@ int solution(string begin, string target, vector<string> words) {
     
     return 0;
 }
+*/
