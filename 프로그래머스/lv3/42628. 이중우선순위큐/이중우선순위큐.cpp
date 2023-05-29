6ff61332194c9cc36b8349291fc5bf42255bf77c
jaemin <69967974+als982001@users.noreply.github.com>
2023-05-29 20:08:46 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -14,6 +14,57 @@ vector<int> solution(vector<string> operations) {
     for (int i = 0; i < operations.size(); ++i)
     {
         istringstream operation(operations[i]);
+        
+        string cmd, val;
+        operation >> cmd >> val;
+        
+        if (cmd == "I")
+        {
+            dq.push_back(stoi(val));   
+            sort(dq.begin(), dq.end());
+        }
+        else if (val == "1")
+        {
+            if (dq.empty() == false)
+                dq.pop_back();
+        }
+        else if (val == "-1")
+        {
+            if (dq.empty() == false)
+                dq.pop_front();
+        }
+    }
+    
+    if (dq.empty())
+        answer = { 0, 0 };
+    else
+    {
+        answer.push_back(dq.back());
+        answer.push_back(dq.front());
+    }
+    
+
+    
+    return answer;
+}
+
+/*
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <deque>
+#include <sstream>
+
+using namespace std;
+
+vector<int> solution(vector<string> operations) {
+    vector<int> answer;
+    
+    deque<int> dq;
+    
+    for (int i = 0; i < operations.size(); ++i)
+    {
+        istringstream operation(operations[i]);
         string cmd, num;
         
         operation >> cmd >> num;
@@ -42,3 +93,4 @@ vector<int> solution(vector<string> operations) {
     else
         return { dq.back(), dq.front() };
 }
+*/
