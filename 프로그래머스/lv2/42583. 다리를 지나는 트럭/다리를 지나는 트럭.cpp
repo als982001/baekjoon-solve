b268a916beeda8a3fe1c5496123e2dd6a30b6c7f
jaemin <69967974+als982001@users.noreply.github.com>
2023-06-12 20:40:48 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -7,11 +7,12 @@ using namespace std;
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int answer = 0;
     
-    int truckIdx = 0;
-    int current = 0;
-    int totalWeight = 0;
+    int truckIdx = 0;       // 트럭의 인덱스
+    int current = 1;        // 현재 시간 (경과 시간 1부터 트럭이 다리에 올라갈 수 있다.)
+    int totalWeight = 0;    // 다리 위의 트럭들 무게의 합
     queue<pair<int, int>> trucksOnBridge; // first: 무게, second: 다리에 올라온 시간
     
+    // 모든 트럭이 다리에 올라가지 못했거나, 다리 위에 트럭이 존재하는 경우 반복문을 진행한다.
     while(truckIdx < truck_weights.size() || trucksOnBridge.size() > 0)
     {   
         // 이미 다리를 건넌 트럭은 다리 위에서 제거한다.
@@ -19,6 +20,7 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
         {
             pair<int, int> truck = trucksOnBridge.front();
             
+            // 현재 시간 - 트럭이 다리에 올라간 시간 >= 다리 길이 -> 다리를 건넘
             if (current - truck.second >= bridge_length)
             {
                 trucksOnBridge.pop();
@@ -28,22 +30,26 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
                 break;
         }
         
+        // 만약 현재 트럭의 인덱스가 주어진 트럭의 범위 안이라면
+        // 트럭이 다리에 올라갈 수 있는지 검사한다.
         if (truckIdx < truck_weights.size())
         {
             int currentTruck = truck_weights[truckIdx];
-        
+            
+            // 만약 다리에 트럭이 올라갈 공간이 있으면서
+            // 현재 다리 위의 트럭들의 총 무게 + 현재 트럭 무게 <= 주어진 무게 조건 이라면 다리로 올라갈 수 있다.
             if (trucksOnBridge.size() < bridge_length && totalWeight + currentTruck <= weight)
             {
-                trucksOnBridge.push({ currentTruck, current });
-                totalWeight += currentTruck;
-                 ++truckIdx;
+                trucksOnBridge.push({ currentTruck, current }); // 다리에 올라간 것을 표현
+                totalWeight += currentTruck;                    // 총 무게에 현재 트럭의 무게를 더해준다.
+                 ++truckIdx;                                    // 다음 트럭을 가리킴
             }
         }
         
-        ++current;
+        ++current;  // 시간이 1 경과한다.
     }
         
-    answer = current;
+    answer = current - 1;   // 마지막 차례에서 ++current; 한 거를 빼줘야 한다.
     
     return answer;
 }
