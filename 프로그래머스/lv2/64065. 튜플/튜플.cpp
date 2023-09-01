56d700afa64d70b53f5a98aa64b53a9f77b7135f
jaemin <69967974+als982001@users.noreply.github.com>
2023-09-01 16:43:53 +0900

    [level 2] Title: 튜플, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.cpp
+++ 프로그래머스/lv2/64065. 튜플/튜플.cpp
@@ -57,13 +57,13 @@ vector<vector<int>> Parse(string s)
 vector<int> solution(string s) {
     vector<int> answer;
 
-    vector<vector<int>> tuple = Parse(s);
+    vector<vector<int>> sets = Parse(s);
     
-    sort(tuple.begin(), tuple.end(), Compare);
+    sort(sets.begin(), sets.end(), Compare);
     
-    for (int i = 0; i < tuple.size(); ++i)
+    for (int i = 0; i < sets.size(); ++i)
     {
-        vector<int> curTuple = tuple[i];
+        vector<int> curTuple = sets[i];
         
         for (int k = 0; k < curTuple.size(); ++k)
         {
@@ -82,76 +82,3 @@ vector<int> solution(string s) {
     
     return answer;
 }
-
-/*
-#include <string>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-bool check[100001];
-
-bool Compare(vector<int> a, vector<int> b)
-{
-	if (a.size() < b.size())
-		return true;
-	else
-		return false;
-}
-
-vector<int> solution(string s)
-{
-	vector<int> answer;
-
-	int idx = 1;
-
-	vector<vector<int>> numsVec;
-
-	while (idx < s.size() - 1)
-	{
-		if (s[idx] == '{')
-		{
-			vector<int> nums;
-
-			while (s[idx] != '}')
-			{
-				++idx;
-				string num = "";
-
-				while ('0' <= s[idx] && s[idx] <= '9')
-				{
-					num += s[idx];
-					++idx;
-				}
-
-				nums.push_back(stoi(num));
-			}
-
-			numsVec.push_back(nums);
-		}
-		else
-			++idx;
-	}
-
-	sort(numsVec.begin(), numsVec.end(), Compare);
-
-	for (int i = 0; i < numsVec.size(); ++i)
-	{
-		for (int k = 0; k < numsVec[i].size(); ++k)
-		{
-			int num = numsVec[i][k];
-
-			if (!check[num])
-			{
-				check[num] = true;
-				answer.push_back(num);
-
-				break;
-			}
-		}
-	}
-	
-
-	return answer;
-}
-*/
