72e53d4584aaaad08a54b673e0952809f3ea5163
jaemin <69967974+als982001@users.noreply.github.com>
2023-09-03 16:26:01 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.04 ms, Memory: 3.7 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.cpp
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.cpp
@@ -1,89 +1,75 @@
-/*
 #include <string>
 #include <vector>
-
+#include <map>
+#define LEFT 'L'
+#define RIGHT 'R'
 using namespace std;
 
-string solution(vector<int> numbers, string hand) {
-    string answer = "";
-    return answer;
+map<int, pair<int, int>> coordinate;
+
+int Distance(pair<int, int> a, pair<int, int> b)
+{
+    return abs(a.first - b.first) + abs(a.second - b.second);
 }
-*/
-
-#include <string>
-#include <vector>
-
-using namespace std;
 
 string solution(vector<int> numbers, string hand) {
     string answer = "";
     
-    int Lr = 3, Lc = 0;
-    int Rr = 3, Rc = 2;
-
-    for (int num = 0; num < numbers.size(); ++num)
+    char defaultHand = hand == "left" ? LEFT : RIGHT;
+    
+    coordinate[1] = { 0, 0 }, coordinate[2] = { 0, 1 }, coordinate[3] = { 0, 2 };
+    coordinate[4] = { 1, 0 }, coordinate[5] = { 1, 1 }, coordinate[6] = { 1, 2 };
+    coordinate[7] = { 2, 0 }, coordinate[8] = { 2, 1 }, coordinate[9] = { 2, 2 };
+    coordinate[0] = { 3, 1 };
+    
+    pair<int, int> leftHand = { 3, 0 };
+    pair<int, int> rightHand = { 3, 2 };
+    
+    for (int i = 0; i < numbers.size(); ++i)
     {
-        if (numbers[num] % 3 == 2 || numbers[num] == 0)
+        int number = numbers[i];
+                
+        if (number == 1 || number == 4 || number == 7)
         {
-            int target = numbers[num];
-            int target_r, target_c;
-
-            if (target != 0)
+            leftHand = coordinate[number];
+            answer += LEFT;
+        }
+        else if (number == 3 || number == 6 || number == 9)
+        {
+            rightHand = coordinate[number];
+            answer += RIGHT;
+        }
+        else
+        {
+            int leftHandDistance = Distance(leftHand, coordinate[number]);
+            int rightHandDistance = Distance(rightHand, coordinate[number]);
+            
+            if (leftHandDistance < rightHandDistance)
             {
-
-                target_r = (target - 1) / 3;
-                target_c = (target - 1) % 3;
+                leftHand = coordinate[number];
+                answer += LEFT;
+            }
+            else if (leftHandDistance > rightHandDistance)
+            {
+                rightHand = coordinate[number];
+                answer += RIGHT;
             }
             else
-                target_r = 3, target_c = 1;
-            
-            int left_distance = abs(target_r - Lr) + (target_c - Lc);
-            int right_distance = abs(target_r - Rr) + (Rc - target_c);
-
-            if (left_distance == right_distance)
             {
-                if (hand == "right")
+                if (defaultHand == LEFT)
                 {
-                    answer.push_back('R');
-                    Rr = target_r;
-                    Rc = target_c;
-
+                    leftHand = coordinate[number];
+                    answer += LEFT;
                 }
                 else
                 {
-                    answer.push_back('L');
-                    Lr = target_r;
-                    Lc = target_c;
+                    rightHand = coordinate[number];
+                    answer += RIGHT;
                 }
             }
-            else if (left_distance < right_distance)
-            {
-                answer.push_back('L');
-                Lr = target_r;
-                Lc = target_c;
-            }
-            else
-            {
-                answer.push_back('R');
-                Rr = target_r;
-                Rc = target_c;
-            }
-        }
-        else if (numbers[num] % 3 == 1)
-        {
-            // 1일 때
-            answer.push_back('L');
-            Lr = (numbers[num] - 1) / 3;  // Lr = 0
-            Lc = (numbers[num] - 1) % 3;  // Lc = 0
-        }
-        else if (numbers[num] % 3 == 0)
-        {
-            // 3일 때
-            answer.push_back('R');
-            Rr = (numbers[num] - 1) / 3;  // Rr = 0
-            Rc = (numbers[num] - 1) % 3;  // Rc = 2
         }
     }
+    
     
     return answer;
 }
