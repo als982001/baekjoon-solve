6757e0d7be70c7c3a1ef7fd0390373e1b13d5fb6
jaemin <69967974+als982001@users.noreply.github.com>
2024-02-04 14:59:18 +0900

    [Silver V] Title: 타일 장식물, Time: 0 ms, Memory: 2020 KB, Score: 74 point -BaekjoonHub

---
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+pair<long long, long long> dp[80];
+
+pair<long long, long long> Check(int num)
+{
+    if (dp[num].first != -1)
+        return dp[num];
+
+    pair<long long, long long> befRec = Check(num - 1);
+
+    return dp[num] = { befRec.first + befRec.second, befRec.first };
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+	std::cout.tie(NULL);    
+	std::cin.tie(NULL);
+
+    for (int i = 1; i <= 80; ++i)  
+        dp[i] = { -1, -1 };
+
+    dp[1] = { 1, 1 };
+    dp[2] = { 2, 1 };
+
+    cin >> N;
+
+    pair<long long, long long> rectangle = Check(N);
+
+    long long answer =  (rectangle.first + rectangle.second) * 2;
+
+    cout << answer << endl;
+
+    return 0;
+}
