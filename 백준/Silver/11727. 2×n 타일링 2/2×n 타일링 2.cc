f5009411eca6daf8aa8e0b2cbbf8ec0b0a66621f
jaemin <69967974+als982001@users.noreply.github.com>
2024-02-04 14:35:14 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int arr[1001] = { 0, };
+
+int find(int n)
+{
+	if (n == 1)
+		return 1;
+	if (n == 2)
+		return 3;
+	if (arr[n] != 0)
+		return arr[n];
+	return arr[n] = (find(n - 1) + 2 * find(n - 2)) % 10007;
+	
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << find(n) << endl;
+	return 0;
+}
