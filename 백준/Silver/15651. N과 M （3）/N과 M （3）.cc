3d9bd150530ab895ea6604d1c267958ab23a9bad
jaemin <69967974+als982001@users.noreply.github.com>
2024-11-30 15:48:57 +0900

    [Silver III] Title: N과 M (3), Time: 400 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+bool check[10] = { false, };
+int arr[10];
+
+void Solve(int idx, int currentNum)
+{
+    if (idx == m) 
+    {
+        for (int i = 0; i < m; ++i)
+            printf("%d ", arr[i]);
+        
+        cout << "\n";
+        return;
+    }
+    
+    for (int num = 1; num <= n; ++num)
+    {
+        check[num] = true;
+        arr[idx] = num;
+        Solve(idx + 1, num + 1);
+        check[num] = false;
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    
+    Solve(0, 1);
+    
+    
+    return 0;
+}
