43b5dd0b15c7b61a5322ccb38c67f8fbaf594260
jaemin <69967974+als982001@users.noreply.github.com>
2024-12-01 14:35:39 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 104 ms, Memory: 8708 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#define MAX 100001
+
+using namespace std;
+
+int nodesNum, edgesNum, startNode, visitOrder = 1;
+int visitOrders[MAX];
+vector<int> linkedNodes[MAX];
+
+bool Compare(int a, int b)
+{
+    return a > b;
+}
+
+void BFS()
+{
+    queue<int> q;
+    
+    q.push(startNode);
+    visitOrders[startNode] = visitOrder++;
+    
+    while(!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+
+        for (int i = 0; i < linkedNodes[node].size(); ++i)
+        {
+            int nextNode = linkedNodes[node][i];
+            
+            if (visitOrders[nextNode] == 0)
+            {
+                visitOrders[nextNode] = visitOrder++;
+                q.push(nextNode);
+            } 
+        }
+    }
+    
+    return;
+}
+
+int main()
+{
+    scanf("%d %d %d", &nodesNum, &edgesNum, &startNode);
+    
+    for (int i = 0; i < edgesNum; ++i)
+    {
+        int nodeA, nodeB;
+        scanf("%d %d", &nodeA, &nodeB);
+        
+        linkedNodes[nodeA].push_back(nodeB);
+        linkedNodes[nodeB].push_back(nodeA);
+    }
+    
+    for (int node = 1; node <= nodesNum; ++node)
+        sort(linkedNodes[node].begin(), linkedNodes[node].end(), Compare);
+        
+    BFS();
+    
+    for (int node = 1; node <= nodesNum; ++node)
+        printf("%d\n", visitOrders[node]);
+
+    return 0;
+}
