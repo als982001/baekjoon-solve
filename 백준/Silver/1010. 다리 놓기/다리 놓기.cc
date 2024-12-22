3009729e91cd630b3764faadaaca259671ec4e84
jaemin <69967974+als982001@users.noreply.github.com>
2024-12-22 13:24:20 +0900

    [Silver V] Title: 다리 놓기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.cc
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -1,59 +1,43 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <queue>
 #include <memory.h>
-#include <deque>
-#include <cmath>
-#include <stack>
-#include <cstring>
-#include <typeinfo>
-#include <iomanip>
-#include <limits.h> 
-#include <map>
-#pragma warning(disable:4996)
+#define MAX 31
 
 using namespace std;
 
-#define MAX 31
-
-int testCaseNum;
-int westSiteNum, eastSiteNum;
+int T, N, M;
 int dp[MAX][MAX];
-vector<int> answer;
+
+int Solve()
+{
+    for (int eastIdx = 1; eastIdx <= M; ++eastIdx)
+        dp[1][eastIdx] = eastIdx;
+        
+    for (int westIdx = 2; westIdx <= N; ++westIdx)
+    {
+        for (int eastIdx = westIdx; eastIdx <= M; ++eastIdx)
+        {
+            for (int targetEastIdx = eastIdx; westIdx <= targetEastIdx; --targetEastIdx)
+                dp[westIdx][eastIdx] += dp[westIdx - 1][targetEastIdx - 1];
+        }
+    }
+    
+    return dp[N][M];
+}
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL);
-	std::cout.tie(NULL);	
-
-	cin >> testCaseNum;
-
-	while(testCaseNum--)
-	{
-		memset(dp, 0, sizeof(dp));
-
-		cin >> westSiteNum >> eastSiteNum;
-
-		for (int eastSite = 1; eastSite <= eastSiteNum; ++eastSite)
-			dp[1][eastSite] = eastSite;
-
-		for (int westSite = 2; westSite <= westSiteNum; ++westSite)
-		{
-			for (int eastSite = westSite; eastSite <= eastSiteNum; ++eastSite)
-			{
-				for (int eastSiteIdx = eastSite; eastSiteIdx >= westSite; --eastSiteIdx)
-					dp[westSite][eastSite] += dp[westSite - 1][eastSiteIdx - 1];
-			}
-		}
-
-		answer.push_back(dp[westSiteNum][eastSiteNum]);
-	}
-
-	for (int i = 0; i < answer.size(); ++i)
-		cout << answer[i] << endl;
-	
-	return 0;
+    scanf("%d", &T);
+    
+    while(T--)
+    {
+        memset(dp, 0, sizeof(dp));
+        
+        scanf("%d %d", &N, &M);
+        
+        int answer = Solve();
+        
+        printf("%d\n", answer);
+    }
+    
+    return 0;
 }
