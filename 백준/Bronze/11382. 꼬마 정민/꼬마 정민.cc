d8134be61e12cf7efb4689736ddae40d4bbdf155
jaemin <69967974+als982001@users.noreply.github.com>
2025-06-07 10:16:58 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+
+    long long a, b, c;
+
+    cin >> a >> b >> c;
+
+    cout << a + b + c << endl;
+    
+    return 0;
+}
