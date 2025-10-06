e13ad0c624717b141f49894362d5fb55507e8b68
jaemin <69967974+als982001@users.noreply.github.com>
2025-10-06 16:21:19 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
@@ -6,20 +6,23 @@ using namespace std;
 int solution(vector<int> num_list) {
     int answer = 0;
     
-    int onlyEvenNum = 0;    // 짝수만 이어 붙인 수
-    int onlyOddNum = 0;     // 홀수만 이어 붙인 수
+    string evenNumbers = "";
+    string oddNumbers = "";
     
     for (int i = 0; i < num_list.size(); ++i)
     {
         int num = num_list[i];
         
         if (num % 2 == 0)
-            onlyEvenNum = (onlyEvenNum * 10) + num;
+            evenNumbers = evenNumbers += to_string(num);
         else
-            onlyOddNum = (onlyOddNum * 10) + num;
+            oddNumbers = oddNumbers += to_string(num);
     }
     
-    answer = onlyEvenNum + onlyOddNum;
+    int evenNumber = evenNumbers == "" ? 0 : stoi(evenNumbers);
+    int oddNumber = oddNumbers == "" ? 0 : stoi(oddNumbers);
+    
+    answer = evenNumber + oddNumber;
     
     return answer;
 }
