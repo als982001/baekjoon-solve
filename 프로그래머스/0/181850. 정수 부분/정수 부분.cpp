a3be5cb7bb53f65b1a7e6ed036e5ab417e7ddc4d
jaemin <69967974+als982001@users.noreply.github.com>
2025-10-06 16:46:54 +0900

    [level 0] Title: 정수 부분, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(double flo) {
+    int answer = 0;
+    
+    answer = static_cast<int>(floor(flo));
+    
+    return answer;
+}
