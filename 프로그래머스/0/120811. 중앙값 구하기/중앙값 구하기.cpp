35901e8f5075cce1620a6dbb530da3ef24b1329d
jaemin <69967974+als982001@users.noreply.github.com>
2025-10-10 15:31:41 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> array) {
+    int answer = 0;
+    
+    sort(array.begin(), array.end());
+    
+    answer = array[(array.size() - 1) / 2];
+    
+    return answer;
+}
