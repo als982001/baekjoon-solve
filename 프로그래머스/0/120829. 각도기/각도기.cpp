a70b31aa36fe196e2475ed8799132d66922087f4
jaemin <69967974+als982001@users.noreply.github.com>
2025-10-10 14:36:14 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) {
+    if (angle < 90)
+        return 1;
+    
+    if (angle == 90)
+        return 2;
+    
+    if (angle < 180)
+        return 3;
+    
+    if (angle == 180)
+        return 4;
+    
+    return 1;
+}
