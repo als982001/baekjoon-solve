aacbb7fba833cf424788568233d395e842140ddf
jaemin <69967974+als982001@users.noreply.github.com>
2026-05-21 14:20:31 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 2599.55 ms, Memory: 15.9 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
@@ -1,204 +1,86 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <map>
 #include <queue>
+#include <map>
+#include <iostream>
 
 #define MAX 51
-#define IMPOSSIBLE "impossible"
 
 using namespace std;
 
-int R, C;
-int endR, endC;
+map<char, pair<int, int>> dirToCoord;
 char dirs[4] = { 'd', 'l', 'r', 'u' };
 string visited[MAX][MAX];
-map<char, pair<int, int>> coordDirs;
 
-void Init()
-{
-    for (int r = 0; r < MAX; ++r)
-    {
-        for (int c = 0; c < MAX; ++c)
-            visited[r][c] = "";
-    }
-    
-    coordDirs['l'] = { 0, -1 };
-    coordDirs['r'] = { 0, 1 };
-    coordDirs['u'] = { -1, 0 };
-    coordDirs['d'] = { 1, 0 };
-}
-
-bool IsIn(int r, int c)
+bool IsIn(int r, int c, int R, int C)
 {
     return 1 <= r && r <= R && 1 <= c && c <= C;
 }
 
-bool CanChange(int r, int c, string route)
-{   
-    // 더 최신 루트면 return true;
-    if (visited[r][c].size() < route.size())
+bool CanVisit(int r, int c, string path)
+{
+    if (visited[r][c] == "")
         return true;
     
-    // 사전 순으로 했을 때 더 빠르면 true
-    return visited[r][c] > route;
+    if (visited[r][c].size() < path.size())
+        return true;
+    
+    return visited[r][c] > path;
 }
 
-string Check(int startR, int startC, int limit)
-{
-    string result = IMPOSSIBLE;
+string solution(int R, int C, int startR, int startC, int endR, int endC, int targetMove) {
+    string answer = "";
     
-    queue<pair<pair<int, int>, string>> q;
-    q.push({ { startR, startC }, "" });
+    dirToCoord['l'] = { 0, -1 };
+    dirToCoord['r'] = { 0, 1 };
+    dirToCoord['u'] = { -1, 0 };
+    dirToCoord['d'] = { 1, 0 };
+    
+    queue<pair<pair<int, int>, pair<int, string>>> q;
+    q.push({ { startR, startC }, { 0, "" } });
     
     while(!q.empty())
     {
-        int r = q.front().first.first;
-        int c = q.front().first.second;
-        string route = q.front().second;
-        
+        int curR = q.front().first.first;
+        int curC = q.front().first.second;
+        int curMove = -q.front().second.first;
+        string curPath = q.front().second.second;
+    
         q.pop();
         
-        if (route.size() >= limit)
+        if (curMove == targetMove)
         {
-            if (r == endR && c == endC)
+            if (curR == endR && curC == endC)
             {
-                result = route;
-                break;
+                if (answer == "" || answer > curPath)
+                    answer = curPath;
             }
-            else
-                continue;
+            
+            continue;
         }
         
+        // 만약 현재 -> 목표 지점까지 최단으로 가도 targetMove 내에 못 갈 경우 제외
+        if (abs(endR - curR) + abs(endC - curC) + curMove > targetMove)
+            continue;
+           
         for (int i = 0; i < 4; ++i)
         {
-            char dir = dirs[i];
-            pair<int, int> coordDir = coordDirs[dir];
+            pair<int, int> dirCoord = dirToCoord[dirs[i]];
             
-            int nxtR = r + coordDir.first;
-            int nxtC = c + coordDir.second;
-            string nxtRoute = route + dir;
+            int nxtR = curR + dirCoord.first;
+            int nxtC = curC + dirCoord.second;
+            string nxtPath = curPath + dirs[i];
             
-            if (IsIn(nxtR, nxtC) && CanChange(nxtR, nxtC, nxtRoute))
+            if (IsIn(nxtR, nxtC, R, C) && CanVisit(nxtR, nxtC, nxtPath))
             {
-                visited[nxtR][nxtC] = nxtRoute;
-                q.push({ { nxtR, nxtC }, nxtRoute });
+                q.push({ { nxtR, nxtC }, { -(nxtPath.size()), nxtPath }});
+                visited[nxtR][nxtC] = nxtPath;
             }
         }
     }
     
-    return result;
-}
-
-string solution(int n, int m, int x, int y, int r, int c, int k) {
-    Init();
-    
-    R = n;
-    C = m;
-    endR = r;
-    endC = c;
-    
-    string answer = Check(x, y, k);
+    if (answer == "")
+        answer = "impossible";
     
     return answer;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-#include <string>
-#include <vector>
-#include <queue>
-
-using namespace std;
-
-#define MAX 52
-#define IMPOSSIBLE "impossible"
-
-int R, C, limit;
-int nr[4] = { 1, 0, 0, -1 };
-int nc[4] = { 0, -1, 1, 0 };
-string visited[MAX][MAX];
-string dir[4] = { "d", "l", "r", "u" };
-
-bool IsIn(int r, int c)
-{
-	return 1 <= r && r <= R && 1 <= c && c <= C;
-}
-
-string Go(pair<int, int> start, pair<int, int> end)
-{
-	queue<pair<pair<int, int>, string>> q;
-	q.push( { { start.first, start.second }, "" } );
-
-	while(!q.empty())
-	{
-		int curR = q.front().first.first;
-		int curC = q.front().first.second;
-		string curRoute = q.front().second;
-		q.pop();
-
-		if (curRoute.size() >= limit)
-		{
-			if (curR == end.first && curC == end.second)
-				return curRoute;
-			else
-				continue;
-		}
-
-		for (int i = 0; i < 4; ++i)
-		{
-			int nxtR = curR + nr[i];
-			int nxtC = curC + nc[i];
-
-			if (IsIn(nxtR, nxtC))
-			{
-				string nxtRoute = curRoute + dir[i];
-
-				if (visited[nxtR][nxtC] == "" || visited[nxtR][nxtC].size() < nxtRoute.size() || visited[nxtR][nxtC] > nxtRoute)
-				{
-					visited[nxtR][nxtC] = nxtRoute;
-					q.push({ { nxtR, nxtC }, nxtRoute });
-				}
-			}
-		}
-	}
-
-	return IMPOSSIBLE;
-}
-
-string solution(int n, int m, int x, int y, int r, int c, int k) {
-    string answer = "";
-    
-    R = n;
-	C = m;
-	limit = k;
-
-	answer = Go({ x, y }, { r, c });
-    
-    return answer;
-}
-
-
-*/
