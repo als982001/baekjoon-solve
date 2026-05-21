085ba60f2761693dd51a6a2fe86dfaf052d67759
jaemin <69967974+als982001@users.noreply.github.com>
2026-05-21 15:10:31 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 39.68 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
@@ -7,6 +7,114 @@ using namespace std;
 long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) {
     long long answer = 0;
     
+    stack<pair<int, int>> deliveryStk;
+    stack<pair<int, int>> pickupStk;
+    
+    for (int idx = 0; idx < deliveries.size(); ++idx)
+    {
+        if (deliveries[idx] > 0)
+            deliveryStk.push({ idx + 1, deliveries[idx] });
+    }
+    
+    for (int idx = 0; idx < pickups.size(); ++idx)
+    {
+        if (pickups[idx] > 0)
+            pickupStk.push({ idx + 1, pickups[idx] });
+    }
+    
+    while(!deliveryStk.empty() || !pickupStk.empty())
+    {
+        int leftDeliveryCap = cap;
+        int maxDeliveryIdx = 0;
+        
+        int leftPickupCap = cap;
+        int maxPickupIdx = 0;
+        
+        while(leftDeliveryCap > 0 && !deliveryStk.empty())
+        {
+            pair<int, int> deliveryInfo = deliveryStk.top();
+            deliveryStk.pop();
+            
+            if (leftDeliveryCap >= deliveryInfo.second)
+            {
+                leftDeliveryCap -= deliveryInfo.second;
+                
+                if (maxDeliveryIdx < deliveryInfo.first)
+                    maxDeliveryIdx = deliveryInfo.first;
+            }
+            else
+            {
+                deliveryInfo.second -= leftDeliveryCap;
+                leftDeliveryCap = 0;
+                
+                deliveryStk.push(deliveryInfo);
+                
+                if (maxDeliveryIdx < deliveryInfo.first)
+                    maxDeliveryIdx = deliveryInfo.first;
+            }
+        }
+        
+        while(leftPickupCap > 0 && !pickupStk.empty())
+        {
+            pair<int, int> pickupInfo = pickupStk.top();
+            pickupStk.pop();
+            
+            if (leftPickupCap >= pickupInfo.second)
+            {
+                leftPickupCap -= pickupInfo.second;
+                
+                if (maxPickupIdx < pickupInfo.first)
+                    maxPickupIdx = pickupInfo.first;
+            }
+            else
+            {
+                pickupInfo.second -= leftPickupCap;
+                leftPickupCap = 0;
+                
+                pickupStk.push(pickupInfo);
+                
+                if (maxPickupIdx < pickupInfo.first)
+                    maxPickupIdx = pickupInfo.first;
+            }
+        }
+        
+        int maxIdx = max(maxDeliveryIdx, maxPickupIdx);
+        
+        answer += maxIdx;
+    }
+    
+    answer *= 2;
+    
+    return answer;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) {
+    long long answer = 0;
+    
     stack<pair<int, int>> dStk; // 배달 스택
     stack<pair<int, int>> pStk; // 수거 스택
     
@@ -80,3 +188,5 @@ long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) 
     
     return answer;
 }
+
+*/
